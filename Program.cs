b0834c03fb6a6e9d2beba4cf29c9d03f4b8c6839
samir06-06 <rehimov.samir060@gmail.com>
2023-02-23 12:43:33 +0400

    uploaded files

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a = 4;
+int b = 5;
+float c = 6.0f;
+double d = 7.5;
+Console.WriteLine(a > b && c > d);
+Console.WriteLine(++a);
+Console.WriteLine(b++);
+Console.WriteLine(a*b);
---
+++ main.js
@@ -0,0 +1,18 @@
+var m = 3
+var n = 2
+var z = n+m
+console.log(z)
+
+let name = "Better Call Saul"
+let surname = "Goodman"
+console.log(name +" "+ surname)
+
+let Samir = 9999
+let Ferhad = 9999
+let x= (Ferhad>=Samir) ? "frfr" : "are you joking or smth?! >:0"
+console.log(x)
+
+let s = 5
+let y = 4
+y**=s
+console.log(y)
