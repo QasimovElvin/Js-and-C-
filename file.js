f5457a20c2df05ec8d4b295342f76f4e9d2a6185
samir06-06 <rehimov.samir060@gmail.com>
2023-02-23 18:35:50 +0400

    Created Project

---
+++ file.js
@@ -0,0 +1,17 @@
+var string ="Hello"
+var number = 5
+var symbol = 'A'
+var boolean = true
+var name = "Elvin"
+console.log(name)
+
+var x = 15
+ var y = 25
+ var z = x + y
+ console.log(z)
+
+ var x = 15
+  var y = 65 
+  var sum = x + y 
+
+  console.log(sum)
